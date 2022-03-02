5a3caf8cf584250bc660288bf2cae257020b6870
ciptowi <ciptowidarto@gmail.com>
2022-03-02 07:23:06 +0700

    set config db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,14 @@
 // setup database here, change the values to suit your environment
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "password",
-  DB: "postgres",
+  HOST: "127.0.0.1",
+  USER: "cipto",
+  PASSWORD: "123456",
+  DB: "swagger",
   dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
