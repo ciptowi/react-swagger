acd36d3c3dc78d8ff3af38b57fc7b36cb37ed5a6
ciptowi <ciptowidarto@gmail.com>
2022-03-06 09:19:57 +0700

    create docs API swagger

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerJSON = require("./swagger.json");
+const swaggerUI = require("swagger-ui-express");
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 // accept request in form or JSON
 app.use(bodyParser.urlencoded({ extended: true }));
